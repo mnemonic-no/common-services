67012ff50e4d1d28f7139b1ee492015a4f9581a3
Konstantin Müller <konstantin@mnemonic.no>
2017-05-05 07:17:24 +0200

    Merge pull request #5 in JOSS/common-services from ~KONSTANTIN/common-services:feature/ARGUS-7753-InvalidCredentialsException to master
    Squashed commit of the following:
    
    commit 836096a79c41c009895c81861e2e9245c1687ddc
    Author: Konstantin Mueller <konstantin@mnemonic.no>
    Date:   Thu May 4 16:41:57 2017 +0200
    
        ARGUS-7753: Added constructor to InvalidCredentialsException.

--- auth/src/main/java/no/mnemonic/services/common/auth/InvalidCredentialsException.java
+++ auth/src/main/java/no/mnemonic/services/common/auth/InvalidCredentialsException.java
@@ -4,4 +4,8 @@ public class InvalidCredentialsException extends Exception {
 
   private static final long serialVersionUID = -8627039809281340658L;
 
+  public InvalidCredentialsException(String message) {
+    super(message);
+  }
+
 }
