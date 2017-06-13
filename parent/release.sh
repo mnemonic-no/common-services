987ddd2284ce40a6bece0ec8d39f8a6ca6faaf1f
Konstantin Müller <konstantin@mnemonic.no>
2017-06-13 09:05:22 +0200

    Merge pull request #10 in JOSS/common-services from ~KONSTANTIN/common-services:feature/ARGUS-8013-prepare-central to master
    Squashed commit of the following:
    
    commit 71798ef080d245d95b77af033ccc7eafe38a2bcb
    Author: Konstantin Mueller <konstantin@mnemonic.no>
    Date:   Tue Jun 13 08:20:18 2017 +0200
    
        ARGUS-8013: Created maven profiles to publish artifacts internally and externally.

--- parent/release.sh
+++ parent/release.sh
@@ -4,7 +4,7 @@ CURBRANCH=`git rev-parse --abbrev-ref HEAD`
 [ "$CURBRANCH" != "master" ] && echo "Can only release from master" && exit 1
 
 echo "Releasing master"
-mvn clean release:prepare -Dmaven.test.skip=true --batch-mode -DautoVersionSubmodules=true -Dresume=false || exit 1
+mvn release:prepare --batch-mode -Ppublish-internal -Darguments=-DskipTests || exit 1
 
 echo "Pushing to Stash"
 git remote add stash ssh://stash.mnemonic.no:8022/joss/common-services
@@ -12,5 +12,4 @@ git fetch stash master  || exit 1
 git push stash master --tags  || exit 1
 
 echo "Performing release"
-mvn release:perform -Dmaven.test.skip=true --batch-mode -Darguments=-DskipTests || exit 1
-
+mvn release:perform --batch-mode -Ppublish-internal -Darguments=-DskipTests || exit 1
